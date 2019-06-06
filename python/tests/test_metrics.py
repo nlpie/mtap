382ec119908b14fbb961e70e12588c62a6d8d099
Ben Knoll <benknoll@umn.edu>
2019-06-06 12:54:25 -0500

    wip adding subprocess events service

---
+++ python/tests/test_metrics.py
@@ -0,0 +1,27 @@
+from nlpnewt import Events
+from nlpnewt._events_service import EventsServicer
+from nlpnewt.metrics import Accuracy, Metrics
+
+
+def test_accuracy():
+    with Events(stub=EventsServicer()) as events:
+        with events.open_event(event_id='1') as event:
+            doc = event.add_document('test', 'This is some text.')
+            with doc.get_labeler('tested') as tested:
+                tested(0, 5)
+                tested(6, 10)
+                tested(11, 20)
+                tested(21, 29)
+                tested(31, 39)
+
+            with doc.get_labeler('target') as target:
+                target(0, 5)
+                target(6, 10)
+                target(11, 20)
+                target(21, 30)
+                target(31, 39)
+
+            acc = Accuracy()
+            metrics = Metrics(acc, tested='tested', target='target')
+            metrics.process(doc, params={})
+            assert abs(acc.value - 0.8) < 1e-6
