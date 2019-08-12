be3c2a5013c7cefe7746c848fe31059abbf3ee50
Ben Knoll <benknoll@umn.edu>
2019-08-12 15:54:38 -0500

    Updated metrics including fuzzy boundary (#100)

--- python/nlpnewt/metrics.py
+++ python/nlpnewt/metrics.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 """Provides functionality for measuring processor performance against gold standards."""
 from abc import ABC, abstractmethod
-from typing import Dict, Any, Optional, Sequence
+from typing import Dict, Any, Optional, Sequence, Iterable
 
 from nlpnewt.events import Document
 from nlpnewt.label_indices import LabelIndex
+from nlpnewt.labels import Label
 from nlpnewt.processing import DocumentProcessor, processor
 
 
@@ -43,6 +44,7 @@ class Metrics(DocumentProcessor):
         The target / gold standard index.
 
     """
+
     def __init__(self, *metrics: Metric, tested: str, target: str):
         self.tested = tested
         self.target = target
@@ -72,6 +74,7 @@ class Accuracy(Metric):
                  name: str = 'accuracy',
                  mode: str = 'equals',
                  print_debug: bool = False,
+                 boundary_fuzz: int = 0,
                  fields: Optional[Sequence[str]] = ...):
         """An accuracy metric with several options for equivalence.
 
@@ -88,6 +91,9 @@ class Accuracy(Metric):
             same values for its fields.
         print_debug: bool
             If true will print debug information about the misses.
+        boundary_fuzz: int
+            How different the target label boundaries can be from the tested boundaries before it
+            doesn't count as a match.
         fields: Sequence[str]
             The field names to compare for equivalency.
         """
@@ -96,48 +102,48 @@ class Accuracy(Metric):
         self.name = name
         self.mode = mode
         self.print_debug = print_debug
+        self.boundary_fuzz = boundary_fuzz
         self.fields = fields
 
     @property
     def value(self) -> float:
         return self.correct / self.total if self.total > 0 else 1.
 
+    def find_candidates(self, tested_index: LabelIndex, target_label: Label) -> LabelIndex:
+        if self.boundary_fuzz == 0:
+            index = tested_index.at(target_label)
+        else:
+            index = tested_index.inside(
+                target_label.start_index - 1,
+                target_label.end_index + 1
+            ).covering(
+                target_label.start_index + 1,
+                target_label.end_index - 1
+            )
+        return index
+
+    def fields_match(self, tested_label: Label, target_label: Label) -> bool:
+        fields = self.fields if self.fields is not ... else target_label.fields.keys() - {'start_index', 'end_index'}
+        return all(getattr(tested_label, field) == getattr(target_label, field) for field in fields)
+
+    def has_match(self, candidates: LabelIndex, target_label: Label) -> bool:
+        if self.mode == 'equals':
+            return len(candidates) == 1 and self.fields_match(candidates[0], target_label)
+        elif self.mode == 'location':
+            return len(candidates) > 0
+        elif self.mode == 'any':
+            return any(self.fields_match(candidate, target_label) for candidate in candidates)
+
     def update(self, document: Document, tested_index: LabelIndex, target_index: LabelIndex) -> Any:
         correct = 0
         total = 0
         for target_label in target_index:
             total += 1
-            if self.mode == 'equals':
-                if self.fields is not ...:
-                    tested_label = tested_index.at(target_label)
-                    if len(tested_label) == 1 and all(getattr(tested_label[0], field) == getattr(target_label, field) for field in self.fields):
-                        correct += 1
-                    elif self.print_debug:
-                        print_overlapping(document, target_label, tested_index)
-                elif len(tested_index.at(target_label)) == 1 and target_label in tested_index:
-                    correct += 1
-                elif self.print_debug:
-                    print_overlapping(document, target_label, tested_index)
-            if self.mode == 'location':
-                if len(tested_index.at(target_label)) > 0:
-                    correct += 1
-                elif self.print_debug:
-                    print_overlapping(document, target_label, tested_index)
-            if self.mode == 'any':
-                if self.fields is not ...:
-                    tested_label = tested_index.at(target_label)
-                    is_correct = False
-                    for label in tested_label:
-                        if all(getattr(label, field) == getattr(target_label, field) for field in self.fields):
-                            correct += 1
-                            is_correct = True
-                            break
-                    if not is_correct and self.print_debug:
-                        print_overlapping(document, target_label, tested_index)
-                elif target_label in tested_index:
-                    correct += 1
-                elif self.print_debug:
-                    print_overlapping(document, target_label, tested_index)
+            candidates = self.find_candidates(tested_index, target_label)
+            if self.has_match(candidates, target_label):
+                correct += 1
+            elif self.print_debug:
+                print_overlapping(document, target_label, tested_index)
 
         self.correct += correct
         self.total += total
