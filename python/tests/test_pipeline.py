51a46d2b5cc7c54c17e47e534f9c057d63dfa855
Ben Knoll <benknoll@umn.edu>
2020-01-10 12:13:24 -0600

    Fixed multiprocessing test

--- python/tests/test_pipeline.py
+++ python/tests/test_pipeline.py
@@ -11,24 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import time
 from datetime import timedelta
 from typing import Dict, Any
-
-import time
 
 from mtap import Pipeline, LocalProcessor, Event, EventsClient
 from mtap.processing import EventProcessor
 
 
 class Processor(EventProcessor):
+    def __init__(self, identifier='1'):
+        self.identifier = identifier
+
     def process(self, event: Event, params: Dict[str, Any]):
         time.sleep(0.001)
+        event.metadata[self.identifier] = 'True'
 
 
 def test_time_result():
     processor = Processor()
     with Pipeline(
-        LocalProcessor(processor, component_id='test_processor', client=None)
+            LocalProcessor(processor, component_id='test_processor', client=None)
     ) as pipeline:
         event = Event()
         results = pipeline.run(event)
@@ -36,21 +39,19 @@ def test_time_result():
         assert result.timing_info['process_method'] >= timedelta(seconds=0.001)
 
 
-def test_run_async(mocker):
+def test_run_multi(mocker):
     client = mocker.Mock(EventsClient)
     client.get_all_document_names.return_value = ['plaintext']
-    processor1 = Processor()
-    processor2 = Processor()
-    processor3 = Processor()
+    client.get_all_metadata.return_value = {}
+    processor1 = Processor('1')
+    processor2 = Processor('2')
+    processor3 = Processor('3')
     with Pipeline(
-        LocalProcessor(processor1, component_id='processor1', client=client),
-        LocalProcessor(processor2, component_id='processor2', client=client),
-        LocalProcessor(processor3, component_id='processor3', client=client)
+            LocalProcessor(processor1, component_id='processor1', client=client),
+            LocalProcessor(processor2, component_id='processor2', client=client),
+            LocalProcessor(processor3, component_id='processor3', client=client)
     ) as pipeline:
-        futures = []
-        for i in range(10):
-            e = Event()
-            futures.append(pipeline.run_async(e, client))
-        for future in futures:
-            results = future.result(timeout=10)
-            assert len(results) == 3
+        events = [Event() for _ in range(10)]
+        results = pipeline.run_multithread(events, progress=False)
+        for result in results:
+            assert len(result) == 3
