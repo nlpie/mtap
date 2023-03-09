6a7f3f277cdd14038f53c3ab72afeebc7dccbc8e
Ben Knoll <benknoll@umn.edu>
2023-03-09 10:07:47 -0600

    Python dependabot setup
    Also made setuptools run protoc to generate pb2s.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#  Copyright 2023 Regents of the University of Minnesota.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
+
+import importlib_resources
+from setuptools import Command, setup
+from setuptools.command.build_py import build_py as _build_py
+
+
+class protoc(Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import grpc_tools.protoc
+
+        with importlib_resources.as_file(importlib_resources.files('google')) as api_protos:
+            grpc_tools.protoc.main([
+                'grpc_tools.protoc',
+                f'-I{os.fspath(api_protos)}',
+                '--python_out=python/',
+                '--pyi_out=python/',
+                '--grpc_python_out=python/',
+                'protos/mtap/api/v1/*.proto'
+            ])
+
+
+class build_py(_build_py):
+    def run(self):
+        self.run_command('protoc')
+        super().run()
+
+
+setup(
+    cmdclass={
+        "build_py": build_py,
+        "protoc": protoc
+    }
+)
