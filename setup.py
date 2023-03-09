efffe6105605d4d91a1e802ff71f6d4bb25e3400
Ben Knoll <benknoll@umn.edu>
2023-03-09 11:23:40 -0600

    Fixed protoc

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import os
+import shutil
+import tempfile
 
 import importlib_resources
 from setuptools import Command, setup
@@ -28,15 +30,19 @@ class protoc(Command):
     def run(self):
         import grpc_tools.protoc
 
-        with importlib_resources.as_file(importlib_resources.files('google')) as api_protos:
-            grpc_tools.protoc.main([
-                'grpc_tools.protoc',
-                f'-I{os.fspath(api_protos)}',
-                '--python_out=python/',
-                '--pyi_out=python/',
-                '--grpc_python_out=python/',
-                'protos/mtap/api/v1/*.proto'
-            ])
+        with (importlib_resources.as_file(importlib_resources.files('google')) as api_protos,
+              tempfile.TemporaryDirectory() as tempdir):
+            shutil.copytree(os.fspath(api_protos), os.path.join(tempdir, 'google'))
+            for proto in ['events.proto', 'processing.proto']:
+                grpc_tools.protoc.main([
+                    'grpc_tools.protoc',
+                    f'-I{tempdir}',
+                    '-Iproto/',
+                    '--python_out=python/',
+                    '--pyi_out=python/',
+                    '--grpc_python_out=python/',
+                    f'proto/mtap/api/v1/{proto}'
+                ])
 
 
 class build_py(_build_py):
